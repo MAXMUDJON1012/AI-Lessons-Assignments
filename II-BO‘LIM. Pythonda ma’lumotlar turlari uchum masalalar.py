dca50f9252733dce7432ccb6169dbbaaf0679d64
maxmudjon <maxmudjonmirzoxidovich@gmail.com>
2026-03-02 09:54:07 +0500

    2_ASSIGNMENTS

---
+++ II-BO‘LIM. Pythonda ma’lumotlar turlari uchum masalalar.py
@@ -0,0 +1,8 @@
+                                             # 1.PART
+# 2.question
+isim=input("Ismingiz nima?")
+print(isim.lower())
+#3.question
+familiya=input("familyangiz nima?")
+print(len(familiya) and familiya.__len__())
+print(familiya[-1])
